5ba954ef250d414ac55b545fbacb9327647a1a6c
Nicole Jimenez <jas.jimenez411@gmail.com>
2021-06-01 22:58:52 +0200

    feat(Model): Create model classes

---
+++ src/model/City.java
@@ -0,0 +1,9 @@
+package model;
+
+public class City {
+    private String name;
+    private String address;
+    private String country;
+    private double latitude;
+    private double longitude;
+}
