602916b3f487453135722b06119bfa13e98ecedb
Nicole Jimenez <jas.jimenez411@gmail.com>
2021-06-02 18:05:37 +0200

    feat(JsonReader): Parse json file

--- src/main.java
+++ src/main.java
@@ -1,6 +1,16 @@
+import model.Routes;
+import utils.JSONReader;
+
 public class main {
+    public static final String PATH = "resources/dades_routes_1.json";
 
     public static void main(String[] args) {
+        JSONReader jsonReader = new JSONReader();
 
+        // Get routes
+        jsonReader.getDadesRoutes(PATH);
+        Routes routes = new Routes(jsonReader.getCities(), jsonReader.getConnections());
+
+        System.out.println("Start the program");
     }
 }
